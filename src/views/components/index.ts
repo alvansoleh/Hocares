9e63586fa9b2093c78da428a505b457316f61052
Al Ghifary A N <132826203+AlGhifaryAN@users.noreply.github.com>
2024-05-14 00:24:53 +0700

    Added latest front end files

---
+++ src/views/components/index.ts
@@ -0,0 +1,14 @@
+import VComponentLoader from "./loader"
+import VComponentToast from "./toast"
+import VComponentModal from "./modal";
+import VComponentStat from "./stat";
+
+export * from "./layouts"
+export * from "./form";
+export * from "./table";
+export {
+    VComponentLoader,
+    VComponentToast,
+    VComponentModal,
+    VComponentStat
+}
